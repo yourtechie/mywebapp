386f77600374e8329d78bcbbf077a7e253b8f1c8
Brookeycutie <brookeyajayi55@gmail.com>
2024-08-10 14:30:15 +0100

    Working index.php

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hello Mary!!!! It worked</title>
+    <title>Hello Mary!!!!</title>
 </head>
 <body>
-    
+    <p>Hi Mary. . .  I am working</p>
 </body>
 </html>
